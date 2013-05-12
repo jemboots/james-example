8a27cdbf22c7f69c5d545a2dde7c4cf337f655c9
James.liu.proj <James.liu.proj@gmail.com>
2013-05-12 09:22:57 +0000

    add support to 3rd view

---
+++ HorizontalSlidingSegueExample/HorizontalSlidingSegueExample/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  HorizontalSlidingSegueExample
+//
+//  Created by James on 5/12/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
