f5d7ecbccfc1c834d15a4fc78fda14786d85e9c0
James.liu.proj <James.liu.proj@gmail.com>
2013-06-29 09:07:48 +0000

    remove warning

--- android/DragToRefresh/src/com/jms/dragtorefresh/RefreshableListView.java
+++ android/DragToRefresh/src/com/jms/dragtorefresh/RefreshableListView.java
@@ -3,7 +3,6 @@ package com.jms.dragtorefresh;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import android.R.bool;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
